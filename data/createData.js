c696c09592de0b9d255c6b3310ce1669d0cd1ab1
Louis Cassany <louis.Cassany@beyond-the-sea.fr>
2024-09-23 16:36:39 +0200

    More regex

--- data/createData.js
+++ data/createData.js
@@ -14,8 +14,9 @@ const filenames = fs.readdirSync(in_path);
 for (const category of filenames) {
     const HTMfiles = fs.readdirSync(`${in_path}/${category}`);
     for (const HTMfile of HTMfiles) {
-        out[HTMfile.replace(".htm", "")] = {};
-        out[HTMfile.replace(".htm", "")].category = category;
+        const itemKey = HTMfile.replace(".htm", "")
+        out[itemKey] = {};
+        out[itemKey].category = category;
         const dataFR = fs.readFileSync(
             `${in_path}/${category}/${HTMfile}`,
             "utf-8"
@@ -28,16 +29,20 @@ for (const category of filenames) {
             if (line.length != 0 && !reachedHeaderEnd) {
                 const key = line.split(":")[0];
                 const value = line.split(":")[1].trim();
-                out[HTMfile.replace(".htm", "")][key] = value;
+                out[itemKey][key] = value;
             }
             if (reachedHeaderEnd) break;
         }
         const descriptionRegex =
             /[-]{2,} Desc \(en\) [-]{2,}([\s\S]*?)[-]{2,} Desc \(fr\) [-]{2,}([\s\S]*?)[-]{2,} End desc [-]{2,}/gm;
+        const patterns = [/@Damage\[[^{]+{([^}]+)}/gm, /@UUID\[[^\]]+\]{([^}]+)/gm, /@[Cc]heck\[(\w+)[^\]]+\]/gm, /@Template\[[^\]]+\]{([^}]+)}/gm]
         const match = descriptionRegex.exec(dataFR);
         if (match) {
-            out[HTMfile.replace(".htm", "")]["descEN"] = match[1].trim();
-            out[HTMfile.replace(".htm", "")]["descFR"] = match[2].trim();
+
+            // const descEN = replaceWithLastCapture(patterns, match[1].trim());
+            const descFR = replaceWithLastCapture(patterns, match[2].trim());
+            // out[itemKey]["descEN"] = descEN;
+            out[itemKey]["descFR"] = descFR;
         } else {
             console.log("Error capturing descriptions in: ", category, HTMfile);
         }
@@ -49,7 +54,7 @@ for (const category of filenames) {
                 const key = keyValue.split(":")[0].trim();
                 const value = keyValue.split(":")[1];
                 if (value != undefined) {
-                    out[HTMfile.replace(".htm", "")][key] = value.trim();
+                    out[itemKey][key] = replaceWithLastCapture(patterns, value.trim());
                 }
             }
         }
@@ -60,3 +65,16 @@ fs.writeFileSync(
     `${out_path}/data.ts`,
     `export const data = ${JSON.stringify(out, null, 2)}`
 );
+
+function replaceWithLastCapture(patterns, text) {
+    patterns.forEach(regex => {
+        text = text.replace(regex, (...matches) => {
+            const captureGroups = matches.slice(1, -2)
+            return captureGroups[1] ? `<b>${captureGroups[1]}</b>` : `<b>${captureGroups[0]}</b>`;
+        });
+    });
+    return text
+}
+
+// Check class-14-2HMvAB6mIVwvwtjT, its wrong
+
