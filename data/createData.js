7a9232a0e4047f21f2c7954ac1ece549f407b34d
Louis Cassany <louis.Cassany@beyond-the-sea.fr>
2024-10-01 13:46:51 +0200

    Adding en spells system

--- data/createData.js
+++ data/createData.js
@@ -1,4 +1,5 @@
-import fs from 'fs';
+import { log } from 'console';
+import fs, { cp } from 'fs';
 
 
 const in_path = "./datasource"
@@ -58,6 +59,23 @@ for (const category of filenames) {
                 }
             }
         }
+    }
+}
+
+// Read all the JSON files in the "./spells_en" folder and add them to a spells_en array
+const spells_en = [];
+const spells_en_path = "./spells_en";
+const spells_en_filenames = fs.readdirSync(spells_en_path);
+for (const spell_en_filename of spells_en_filenames) {
+    const spell_en = JSON.parse(fs.readFileSync(`${spells_en_path}/${spell_en_filename}`, "utf-8"));
+    spells_en.push(spell_en);
+}
+
+// Iterate over the spells_en array and find the corresponding item in the out object by checking if the "_id" property is included in the item key
+for (const spell_en of spells_en) {
+    const itemKey = Object.keys(out).find(key => key.includes(spell_en._id));
+    if (itemKey) {
+        out[itemKey].spell_en = spell_en;
     }
 }
 
